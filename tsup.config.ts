a3fe55762716a8af0a3c17c99e3d788f237519ee
Frodi1998 <rafael.abdulov.650@gmail.com>
2024-02-17 17:13:42 +0500

    build: update tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,11 +2,15 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   name: 'tsup',
-  target: 'node14',
+  platform: 'node',
+  target: 'es2022',
   splitting: true,
+  skipNodeModulesBundle: true,
+  clean: false,
   entry: ['./src/index.ts'],
   format: ['cjs', 'esm'],
   dts: {
     resolve: true,
   },
+  treeshake: false,
 });
