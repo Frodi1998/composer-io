3020283c87964ff3c94c8a3e0950915661d1d0c1
Frodi1998 <rafael.abdulov.650@gmail.com>
2024-02-26 19:37:11 +0500

    build: tsup target esnext

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,12 +3,17 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   name: 'tsup',
   platform: 'node',
-  target: 'es2022',
+  target: 'esnext',
   splitting: true,
   skipNodeModulesBundle: true,
   clean: true,
   entry: ['./src/index.ts'],
   format: ['cjs', 'esm'],
+  terserOptions: {
+    mangle: false,
+    keep_classnames: true,
+    keep_fnames: true,
+  },
   dts: {
     resolve: true,
   },
